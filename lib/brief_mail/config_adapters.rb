b0c4c10032d21690ac5adeecac912033272b6ffe
S. Christoffer Eliesen <cel@io.no>
2013-05-24 14:00:26 +0200

    brief_mail/config_adapters: Accept config if it responds properly.

--- lib/brief_mail/config_adapters.rb
+++ lib/brief_mail/config_adapters.rb
@@ -3,14 +3,56 @@ require 'brief_mail/config_adapters/capistrano'
 
 module BriefMail
   module ConfigAdapters
+    Error = Class.new RuntimeError
 
-    def self.adapter_for(config)
-      if defined? ::Capistrano::Configuration and config.is_a? ::Capistrano::Configuration::Namespaces::Namespace
-        Capistrano.new(config)
-      else
-        raise %(Unknown config adapter class: %s) % config.class
+    class ArgumentError < Error
+      def initialize(config)
+        super %(Config object (%s) needs to respond to the following methods: %s.) %
+          [config.class, ConfigAdapters.missing_responses(config).join(", ")]
       end
     end
 
+    def self.adapter_for(config)
+      if capistrano?(config)
+        Capistrano.new(config)
+
+      elsif responds_properly?(config)
+        config
+
+      else
+        fail ArgumentError, config
+      end
+    end
+
+    def self.capistrano?(config)
+      defined? ::Capistrano::Configuration and
+        config.is_a? ::Capistrano::Configuration::Namespaces::Namespace
+    end
+
+    def self.responds_properly?(config)
+      missing_responses(config).empty?
+    end
+
+    def self.missing_responses(config)
+      EXPECTED_METHODS.reject do |meth|
+        config.respond_to? meth
+      end
+    end
+
+    private
+
+    EXPECTED_METHODS = [
+      :application,
+      :current_release,
+      :current_revision,
+      :from_user,
+      :mailer,
+      :previous_release,
+      :previous_revision,
+      :recipients,
+      :scm,
+      :stage,
+      :subject,
+    ].freeze
   end
 end
